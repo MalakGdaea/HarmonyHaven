79fcae17b57762b447d656f362658826da09679f
malak <malak.abogdaya@gmail.com>
2025-11-16 12:42:45 +0200

    depoy prep

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
